1ce070bbc5af304da40bf8aa2729ea56137f3414
kouya17 <kouya17@users.noreply.github.com>
2023-02-26 14:30:35 +0000

    deploy: 21f11414a09bc705b565964f53ac0f234e2bcc7a

---
+++ _next/static/sNjUNuZWx_ceIbElgNFjk/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/76d22f9e-0869cfe776af951a.js","static/chunks/571-addf0b41b2391faa.js","static/chunks/pages/index-b7f9f5b3c00dd26e.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
